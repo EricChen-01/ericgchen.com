7121f33fc1302730fcc1602a4265f537ecb8d7a2
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-03 18:45:09 -0500

    added title and photo

--- src/components/NewPortfolio/FrontPage.js
+++ src/components/NewPortfolio/FrontPage.js
@@ -1,10 +1,27 @@
-import { Box } from "@mui/material";
-
+import { Box, Grid, Typography } from "@mui/material";
+import me from '../../images/me.png';
 
 function FrontPage(){
     return(
         <Box>
-            Front page...
+            <Grid 
+                container
+                height='100vh'
+                direction='column'
+                justifyContent='space-around'
+            >
+                <Box align='center' sx={{pt:30}} border='1px solid red'>
+                    <Typography variant='h2'color='text.primary'>
+                    Hello! My name is Eric
+                    </Typography>
+                    <Typography variant='subtitle1'>
+                        Software Engineer
+                    </Typography>
+                </Box>
+                <Box align='left' border='1px solid blue'>
+                    <img className='round' style={{border:'1px solid black', maxWidth: '300px', width: '100%', height: 'auto'}} src={me} alt='Eric Chen'/>
+                </Box>
+            </Grid>
         </Box>
     )
 }
