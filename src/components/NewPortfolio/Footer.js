191057f2734a346258a4bc35afd8ce08bde7c0fb
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-03 18:40:31 -0500

    added footer component

--- src/components/NewPortfolio/Footer.js
+++ src/components/NewPortfolio/Footer.js
@@ -1,11 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 
 
 function Footer(){
     return(
-        <Box>
-            Footer...
-        </Box>
+    <Box
+      component="footer"
+      py={2} // vertical padding
+      px={4} // horizontal padding
+      textAlign="center"
+      border="1px solid"
+    >
+      <Typography variant="body2" component="div" display="inline-flex" alignItems="center">
+        <CopyrightIcon fontSize="small" />
+        <Box ml={0.5}>Copyright 2026 Eric Chen. All rights reserved.</Box>
+      </Typography>
+    </Box>
     )
 }
 
