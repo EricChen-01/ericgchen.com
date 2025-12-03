fca918b097b2fecda0dc6b53229935939b9e7409
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-03 17:36:22 -0500

    setup notfound

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function NotFoundPage(){
+    return(
+        <Box textAlign='center'>
+            Page not found...
+        </Box>
+    )
+}
+
+export default NotFoundPage;
