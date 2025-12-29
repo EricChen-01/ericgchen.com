4c5518daaf05abfb8c8c49497d32a76ab4b51bac
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-29 15:49:25 -0500

    removed others from index.js

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,7 +1,6 @@
 import {project as iosshopper} from './iOSShopper';
 import {project as kayle} from './Kayle';
 import {project as playlister} from './PlayLister';
-import {writing} from './other';
 import {project as patchclone} from './PatchClone';
 import {project as heap} from './HeapAllicator';
 import {project as tracer} from './BitCoinTracer';
@@ -10,8 +9,7 @@ export const projects = [
     iosshopper,
     playlister,
     kayle,
-    // patchclone,
-    // heap,
-    // tracer,
-    writing
+    //patchclone,
+    //heap,
+    //tracer,
 ];
