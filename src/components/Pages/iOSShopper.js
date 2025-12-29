291bb70ef72736d2a3b45c920a286325d56680f6
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-29 15:46:02 -0500

    added dummy image to projects pages

--- src/components/Pages/iOSShopper.js
+++ src/components/Pages/iOSShopper.js
@@ -5,5 +5,5 @@ export const project = {
     technologies: ['SwiftUI', 'RESTFUL API'],
     isPath: false,
     path:'',
-    image: ""
+    image: "https://cdn.prod.website-files.com/63a5463b6c31f2d40f1d3a47/66aad35d33391259ac56b91b_goose.png"
 }
