c310c5c37ce58fb0e8bd730ee09dd92241ae99bf
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-29 15:01:23 -0500

    generated a project card for each project in the pages folder

--- src/components/NewPortfolio/Projects.js
+++ src/components/NewPortfolio/Projects.js
@@ -1,10 +1,35 @@
 import { Box, Grid, Typography} from "@mui/material";
-import {CenteredDivider, HoverLink} from '../../components';
+import {CenteredDivider, HoverLink, ProjectCard} from '../../components';
+import {projects} from '../Pages';
+import { BorderColor } from "@mui/icons-material";
 
 function Projects() {
     return (
         <Box>
-            <Typography variant="h2">Projects I've worked on</Typography>
+            <Box>
+                <Typography variant="h2" sx={{
+                    textAlign: {xs: 'center', sm: 'left'}, 
+                    pl: {xs: 0, sm: 6},
+                }}
+                >
+                    Projects I've worked on</Typography>
+            </Box>
+            <Grid container spacing={3} sx={{px: { xs: 2, sm: 6 }, pt: 3, pb: 6}}>
+                {
+                    projects.map((project) => (
+                        <Grid item xs={12} md={6} lg={4} xl={3}>
+                            <ProjectCard 
+                                title={project.title}
+                                subtitle={project.subtitle}
+                                description={project.description}
+                                tags={project.technologies}
+                                image={project.image}
+                                sx={{ maxWidth: { xs: '100%', sm: 520, md: 720 }, border:"5px solid", borderColor:"primary.main"}}
+                            />
+                        </Grid>
+                    ))
+                }
+            </Grid>
         </Box>
     )
 }
