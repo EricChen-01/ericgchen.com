25eebfe97a50b36e650b163293415ba6f8c49b11
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-29 21:17:31 -0500

    removed old theme

--- src/App.js
+++ src/App.js
@@ -9,59 +9,6 @@ import portfolioTheme from './themes/PortfolioTheme';
 const linkedinUrl = "https://www.linkedin.com/in/erxcchen/";
 const email = "echen3225@gmail.com";
 
-const theme = createTheme({
-  palette: {
-    text:{
-      main:'#2b2d42'
-    },
-    title:{
-      main: '#D9D9D9'
-    },
-    white:{
-      main:'#ebf2fa'
-    },
-    blue:{
-      main: '#00b4d8',
-      neutral: '#4D5C74'
-    },
-    background:{
-      main: '#ebf2fa'
-    },
-    secondary_background:{
-      main: '#4D5C74'
-    },
-    navbar:{
-      main: '#0d1b2a'
-    }
-  },
-  typography:{
-    fontFamily:"Paytone One",
-    MenuItem: {
-      cursor:'pointer', 
-      fontStyle:'normal',
-      lineHeight:'39px', 
-      fontWeight:'500',
-      fontSize:'20px',
-      textShadow: '0px 4px 4px #000000',
-    },
-    h1: {//title1
-      fontSize:'64px', 
-      fontWeight:600,
-    },
-    h2: {//title2
-      fontSize:'32px', 
-      fontWeight:600,
-    },
-    body1: {//main text
-      fontSize:'1rem',
-      fontWeight:400
-    },
-    body2: {// secondary text
-      fontSize: '1rem',
-      fontWeight:200
-    }
-  }
-});
 
 function PortfolioMain(){
   return(
