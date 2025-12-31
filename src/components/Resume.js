c1a8d3dac2a87afc2b39368939f95e8bf248a457
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-31 18:55:43 -0500

    updated Resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,32 +1,30 @@
-import {useTheme} from '@mui/material/styles';
-import {Box,Typography,Grid, List, ListItem,ListItemText, ListItemIcon} from '@mui/material';
+import {Box,Typography,Grid, List, ListItem,ListItemText, ListItemIcon, IconButton, Tooltip} from '@mui/material';
+import { Home } from '@mui/icons-material';
 import resume from '../images/Eric_Chen_Resume.pdf';
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Resume(){
-    const theme = useTheme();
     window.scrollTo(0, 0);
 
     return(
-        <Box id='Resume' height='100%' bgcolor='secondary_background.main'>
-            <Box pd={15} sx={{position:'relative'}}>
-                <Box align='center'>
-                    <Typography className='no-highlight' sx={{textShadow: '0px 6px 16px rgba(0, 0, 0, 0.1)'}} variant='h1' color='white.main'>
-                        Resume
-                    </Typography>
-                    <Box align='center'>
-                        <Link class='no_underline' style={{color:'gold'}} to={'/'}> click me to go home </Link>
-                    </Box>
-                </Box>
-
-                <Box align='center' height='100%' width='100%'>
-                    <Box height="1250px" width={{xs:'90vw', sm:'90vw', md:'50%'}}border={1}>
-                        <iframe src={resume} width="100%" height="100%"></iframe>
-                    </Box>
-                </Box>
-                
+        <Box>
+            <Box align='center'>
+                <Typography className='no-highlight' variant='h1'>
+                    Resume
+                </Typography>
+                <Tooltip title="Go home">
+                    <IconButton component={NavLink} to='/' color="inherit">
+                        <Home/>
+                    </IconButton>
+                </Tooltip>
             </Box>
-            {/* <BottomWave color='background'/> */}
+
+            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" width="100%">
+                <Box sx={{border: '5px solid', borderColor: 'primary.main'}} width={{ xs: '90%', sm: '90%', md: '90%', lg: '50%'}} height={{ xs: '80vh', md: '85vh' }} boxShadow={3} borderRadius={2} overflow="hidden">
+                    <iframe src={resume} title="Resume" width="100%" height="100%" style={{ border: 'none' }} />
+                </Box>
+            </Box>
+
         </Box>
     )
 }
