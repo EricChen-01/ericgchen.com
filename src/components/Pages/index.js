79a21afee0f0695153bfd30d6297ac9f42aa6b61
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2025-12-30 21:57:56 -0500

    added hopper hacks 2024

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -2,10 +2,12 @@ import {project as playlister} from './PlayLister';
 import {project as minigolfvr} from './MiniGolfVR';
 import {project as GitHubQAChatGPTPlugin} from './GithubSummarizer';
 import {project as jems} from './Jems'
+import {project as hopperhacks2024} from './Hopperhacks2024'
 
 export const projects = [
     GitHubQAChatGPTPlugin,
     minigolfvr,
     jems,
+    hopperhacks2024,
     playlister,
 ];
